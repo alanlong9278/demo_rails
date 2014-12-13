c23cbf869af86dc8a210cb41138e0693db8c78c6
= <=>
2014-12-13 19:11:12 +0800

    second commit

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ActiveRecord::Base
   # attr_accessor :title, :text
   has_many :comments, dependent: :destroy
+  belongs_to :users
+
   validates :title, presence: true,
  					length: { minimum: 3}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   validates :username, :presence => true
   validates :password, :presence => true
   validates :email, :email => true
+  
+  has_many :articles, :dependent => :destroy
 end
